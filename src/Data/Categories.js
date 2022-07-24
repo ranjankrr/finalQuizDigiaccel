962e86c73ab21784c69f1f24f402be8aa2f72a7e
pannelal <ranjankrr7892@gmail.com>
2022-07-24 10:42:04 +0530

    final

---
+++ src/Data/Categories.js
@@ -0,0 +1,12 @@
+const Categories = [
+  {
+    category: "General Knowledge",
+    value: 9,
+  },
+  { category: "Science and Nature", value: 17 },
+  { category: "Computer", value: 18 },
+  { category: "Mathematics", value: 19 },
+  
+];
+
+export default Categories;
